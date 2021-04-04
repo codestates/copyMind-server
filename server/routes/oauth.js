a870e8e17a36b51e79a0936ad63f09e10920fc71
Jmwasd <66477332+Jmwasd@users.noreply.github.com>
2021-04-04 17:25:09 +0900

    refactoring branch 생성

--- server/routes/oauth.js
+++
@@ -1,8 +0,0 @@
-var express = require('express');
-var router = express.Router();
-const controllers = require('../controllers/oauth');
-
-router.post('/github', controllers.githubController)
-router.post('/google', controllers.googleController)
-
-module.exports = router;
