891f368c1d9f5ef8b15f40844a699df00d131737
Jmwasd <66477332+Jmwasd@users.noreply.github.com>
2021-04-10 14:09:29 +0900

    update sign

---
+++ route/copy.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const router = express.Router();
+
+router.post('/getcopy', async (req, res) => {});
+
+router.post('/postcopy', async (req, res) => {});
+
+router.post('/addlike', async (req, res) => {});
+
+router.post('/removelike', async (req, res) => {});
+
+module.exports = router;
---
+++ route/user.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const router = express.Router();
+
+router.get('/userinfo', async (req, res) => {});
+
+router.get('/myposting', async (req, res) => {});
+
+router.get('/bookmark', async (req, res) => {});
+
+module.exports = router;
